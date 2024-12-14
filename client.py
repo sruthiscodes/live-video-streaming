28c078e5e8f24f2a493220997023477821aebde1
Sruthi <contactsruthis@gmail.com>
2024-12-15 04:18:02 +0530

    modify client

--- client.py
+++ client.py
@@ -5,26 +5,18 @@ import struct
 import imutils
 import ssl
 
-# Client socket
-# create an INET, STREAMing socket :
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = "192.168.0.100"#"192.168.1.9"  # Standard loopback interface address (localhost)
-port = 5006  # Port to listen on (non-privileged ports are > 1023)
+host_ip = "192.168.0.100"
+port = 5006
 
-# Wrap the client socket with SSL
 context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
 context.check_hostname = False
 context.verify_mode = ssl.CERT_NONE
 
 ssl_client_socket = context.wrap_socket(client_socket, server_hostname=host_ip)
-
-# Now connect to the web server on the specified port number
 ssl_client_socket.connect((host_ip, port))
 
-# 'b' or 'B' produces an instance of the bytes type instead of the str type
-# used in handling binary data from network connections
 data = b""
-# Q: unsigned long long integer(8 bytes)
 payload_size = struct.calcsize("Q")
 while True:
     while len(data) < payload_size:
